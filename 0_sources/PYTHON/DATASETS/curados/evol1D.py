3115c580467da62fecc3eeb7f2aef1d66c1d1505
temisAP <temisap.98@gmail.com>
2022-06-08 20:14:59 +0200

    Drag segments

--- 0_sources/PYTHON/DATASETS/curados/evol1D.py
+++ 0_sources/PYTHON/DATASETS/curados/evol1D.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backend_bases import MouseButton
+from pynput.mouse import Listener, Button
 import matplotlib as mpl
 from scipy.interpolate import interp1d
 import sys
@@ -107,9 +108,26 @@ def curing_evol1D(self,points=None,REF=None,files=None,val='ss',plot=True):
                 plt.axvline(point,linestyle='--',color='black',label='Point chosen')
 
         else:
-
             global new_points
             new_points = list()
+            global start_point
+            start_point = 0
+            global end_point
+            end_point = 0
+            global key_status
+            key_status = ''
+            global aspan_list
+            aspan_list = []
+            global dragging
+            dragging = False
+
+            def on_click(x, y, button, pressed):
+                if button == Button.left:
+                    global key_status
+                    key_status = 'pressed' if pressed else 'released'
+
+            listener = Listener(on_click = on_click)
+            listener.start()
 
             def on_click(event):
 
@@ -129,8 +147,14 @@ def curing_evol1D(self,points=None,REF=None,files=None,val='ss',plot=True):
                 elif event.button is MouseButton.RIGHT:
                     # Remove all lines
                     for line in ax.lines:
-                        if line.get_color() == 'black':
+                        if line.get_color() == 'black' or line.get_color() == 'tab:orange':
                             line.remove()
+                    # Remove all axvspans
+                    global aspan_list
+                    for aspan in aspan_list:
+                        if aspan.__dict__['_original_facecolor'] == 'tab:orange':
+                            aspan.remove()
+                            aspan_list.remove(aspan)
                     # Update legend box
                     handles, labels = plt.gca().get_legend_handles_labels()
                     by_label = dict(zip(labels, handles))
@@ -141,19 +165,112 @@ def curing_evol1D(self,points=None,REF=None,files=None,val='ss',plot=True):
                     plt.draw()
 
             def on_move(event):
+
+                global new_points
+                global end_point
+                global start_point
+                global key_status
+                global dragging
+
                 # Update title with the point position
-                ax.set_title(f'Point at z = {event.xdata} m')
+                try:
+                    ax.set_title(f'Point at z = {float(event.xdata):.5f} m')
+                except:
+                    ax.set_title('')
 
                 # Delete last green line and draw a new one if is into the figure
                 for line in ax.lines:
                     if line.get_color() == 'g':
                         line.remove()
+                # Try drawing new one (to avoid warning when out of box)
                 try:
                     plt.axvline(x=event.xdata, color='g')
-                    plt.draw()
                 except:
                     pass
 
+                # If control key is stroked start listening
+
+                if key_status == 'pressed':
+
+                    # Now we are dragging
+                    dragging = True
+
+                    # Find last black line
+                    line = ax.lines[-2]
+                    if line.get_color() == 'black':
+                        # Get its position
+                        start_point = line.get_xdata()[0]
+                        # Make it green
+                        line.set_color("tab:green")
+                        # Make it continuous
+                        line.set_linestyle('-')
+                        # Make it thicker
+                        line.set_linewidth(2)
+
+                    # Get current position
+                    end_point = float(event.xdata)
+
+                    # Delete last area drawn
+                    try:
+                        if aspan_list[-1].__dict__['_original_facecolor'] == 'tab:green':
+                            aspan_list[-1].remove()
+                            aspan_list.remove(aspan_list[-1])
+                    except Exception as e:
+                        #print(e)
+                        pass
+
+                    # Draw newarea
+                    aspan_list.append(plt.axvspan(start_point, end_point, alpha=0.3, color='tab:green'))
+
+                    # Update
+                    plt.draw()
+
+                elif key_status == 'released' and dragging == True:
+
+                    # Now we have stop dragging
+                    dragging = False
+
+                    # Delete last temporal area drawn
+                    try:
+                        if aspan_list[-1].__dict__['_original_facecolor'] == 'tab:green':
+                            aspan_list[-1].remove()
+                    except Exception as e:
+                        #print(e)
+                        pass
+
+                    # Print definitive area
+                    plt.axvline(x=start_point, linestyle='-', linewidth=2,color='tab:orange',label='Interval edges')
+                    plt.axvline(x=end_point, linestyle='-', linewidth=2,color='tab:orange',label='Interval edges')
+                    aspan_list.append(plt.axvspan(start_point, end_point, alpha=0.3, color='tab:orange', label='Interval choosen'))
+
+                    # Find green lines (temporal) and make it orange (fixed)
+                    for line in ax.lines:
+                        if line.get_color() == 'tab:green':
+                            # Make it orange
+                            line.set_color("tab:orange")
+                            # Make it continuous
+                            line.set_linestyle('-')
+                            # Make it thicker
+                            line.set_linewidth(2)
+                            # Make label none
+                            line.set_label(None)
+
+                    # Replace start point with a list
+
+                    from copy import deepcopy
+                    new_points[-1] = [deepcopy(start_point), deepcopy(end_point)]
+                    # Restart key
+                    key_status = ''
+
+                    # Update legend box
+                    handles, labels = plt.gca().get_legend_handles_labels()
+                    by_label = dict(zip(labels, handles))
+                    plt.legend(by_label.values(), by_label.keys())
+
+                elif key_status == 'released' and dragging == False:
+                    key_status = ''
+
+                plt.draw()
 
             def when_leaving_axes(event):
                 # Delete the dinamic line line when the mouse leaves the axes
@@ -180,9 +297,11 @@ def curing_evol1D(self,points=None,REF=None,files=None,val='ss',plot=True):
 
     # Compute vals and times
     if points:
+        points_labels = [x for x in points if isinstance(x,float) else x[0]]
+
         # Get all the values along the time of all the points specified
-        all_vals    =   dict.fromkeys(points)
-        all_times   =   dict.fromkeys(points)
+        all_vals    =   dict.fromkeys(points_labels)
+        all_times   =   dict.fromkeys(points_labels)
         for point in points:
 
             vals    = list()
--- 5_Curing/plot_example.py
+++ 5_Curing/plot_example.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.backend_bases import MouseButton
 import matplotlib as mpl
 import numpy as np
+from pynput.mouse import Listener, Button
 
 x = np.linspace(0,100,100)
 y = x*1.2+3
@@ -10,50 +11,187 @@ fig, ax = plt.subplots()
 
 plt.plot(x, y, 'o')
 
-global points
-points = list()
+if True:
+    if True:
+        if True:
 
-def on_click(event):
-    if event.button is MouseButton.LEFT:
-        # Create a vertical line on the point clicked
-        plt.axvline(x=event.xdata, color='r')
-        global points
-        points.append(event.xdata)
-        plt.draw()
-    elif event.button is MouseButton.RIGHT:
-        for line in ax.lines:
-            if line.get_color() == 'r':
-                line.remove()
-        points = list()
-        plt.draw()
+            global new_points
+            new_points = list()
+            global start_point
+            start_point = 0
+            global end_point
+            end_point = 0
+            global key_status
+            key_status = ''
+            global aspan_list
+            aspan_list = []
+            global dragging
+            dragging = False
 
-def on_move(event):
-    # Update title with the point position
-    ax.set_title(f'Point at z = {event.xdata} m')
+            def on_click(x, y, button, pressed):
+                if button == Button.left:
+                    global key_status
+                    key_status = 'pressed' if pressed else 'released'
 
-    # Delete last green line and draw a new one if is into the figure
-    for line in ax.lines:
-        if line.get_color() == 'g':
-            line.remove()
-    try:
-        plt.axvline(x=event.xdata, color='g')
-        plt.draw()
-    except:
-        pass
+            listener = Listener(on_click = on_click)
+            listener.start()
 
+            def on_click(event):
 
-def when_leaving_axes(event):
-    # Delete the dinamic line line when the mouse leaves the axes
-    for line in ax.lines:
-        if line.get_color() == 'g':
-            line.remove()
-    plt.draw()
+                if event.button is MouseButton.LEFT:
+                    # Create a vertical line on the point clicked
+                    plt.axvline(x=event.xdata, linestyle='--',color='black',label='Point chosen')
+                    # Update legend box
+                    handles, labels = plt.gca().get_legend_handles_labels()
+                    by_label = dict(zip(labels, handles))
+                    plt.legend(by_label.values(), by_label.keys())
+                    # Update list
+                    global new_points
+                    new_points.append(event.xdata)
+                    # Draw
+                    plt.draw()
 
-# Connect the click and move events to the on_click and on_move functions
-fig.canvas.mpl_connect('button_press_event', on_click)
-fig.canvas.mpl_connect('motion_notify_event', on_move)
-fig.canvas.mpl_connect('axes_leave_event', when_leaving_axes)
+                elif event.button is MouseButton.RIGHT:
+                    # Remove all lines
+                    for line in ax.lines:
+                        if line.get_color() == 'black' or line.get_color() == 'tab:orange':
+                            line.remove()
+                    # Remove all axvspans
+                    global aspan_list
+                    for aspan in aspan_list:
+                        if aspan.__dict__['_original_facecolor'] == 'tab:orange':
+                            aspan.remove()
+                            aspan_list.remove(aspan)
+                    # Update legend box
+                    handles, labels = plt.gca().get_legend_handles_labels()
+                    by_label = dict(zip(labels, handles))
+                    plt.legend(by_label.values(), by_label.keys())
+                    # Delete list
+                    new_points.clear()
+                    # Draw
+                    plt.draw()
+
+            def on_move(event):
+
+                global new_points
+                global end_point
+                global start_point
+                global key_status
+                global dragging
+
+                # Update title with the point position
+                try:
+                    ax.set_title(f'Point at z = {float(event.xdata):.5f} m')
+                except:
+                    ax.set_title('')
+
+                # Delete last green line and draw a new one if is into the figure
+                for line in ax.lines:
+                    if line.get_color() == 'g':
+                        line.remove()
+                # Try drawing new one (to avoid warning when out of box)
+                try:
+                    plt.axvline(x=event.xdata, color='g')
+                except:
+                    pass
+
+                # If control key is stroked start listening
+
+                if key_status == 'pressed':
+
+                    # Now we are dragging
+                    dragging = True
+
+                    # Find last black line
+                    line = ax.lines[-2]
+                    if line.get_color() == 'black':
+                        # Get its position
+                        start_point = line.get_xdata()[0]
+                        # Make it green
+                        line.set_color("tab:green")
+                        # Make it continuous
+                        line.set_linestyle('-')
+                        # Make it thicker
+                        line.set_linewidth(2)
+
+                    # Get current position
+                    end_point = float(event.xdata)
+
+                    # Delete last area drawn
+                    try:
+                        if aspan_list[-1].__dict__['_original_facecolor'] == 'tab:green':
+                            aspan_list[-1].remove()
+                            aspan_list.remove(aspan_list[-1])
+                    except Exception as e:
+                        #print(e)
+                        pass
+
+                    # Draw newarea
+                    aspan_list.append(plt.axvspan(start_point, end_point, alpha=0.3, color='tab:green'))
+
+                    # Update
+                    plt.draw()
+
+                elif key_status == 'released' and dragging == True:
+
+                    # Now we have stop dragging
+                    dragging = False
+
+                    # Delete last temporal area drawn
+                    try:
+                        if aspan_list[-1].__dict__['_original_facecolor'] == 'tab:green':
+                            aspan_list[-1].remove()
+                    except Exception as e:
+                        #print(e)
+                        pass
+
+                    # Print definitive area
+                    plt.axvline(x=start_point, linestyle='-', linewidth=2,color='tab:orange',label='Interval edges')
+                    plt.axvline(x=end_point, linestyle='-', linewidth=2,color='tab:orange',label='Interval edges')
+                    aspan_list.append(plt.axvspan(start_point, end_point, alpha=0.3, color='tab:orange', label='Interval choosen'))
+
+                    # Find green lines (temporal) and make it orange (fixed)
+                    for line in ax.lines:
+                        if line.get_color() == 'tab:green':
+                            # Make it orange
+                            line.set_color("tab:orange")
+                            # Make it continuous
+                            line.set_linestyle('-')
+                            # Make it thicker
+                            line.set_linewidth(2)
+                            # Make label none
+                            line.set_label(None)
+
+                    # Replace start point with a list
+
+                    from copy import deepcopy
+                    new_points[-1] = [deepcopy(start_point), deepcopy(end_point)]
+                    # Restart key
+                    key_status = ''
+
+                    # Update legend box
+                    handles, labels = plt.gca().get_legend_handles_labels()
+                    by_label = dict(zip(labels, handles))
+                    plt.legend(by_label.values(), by_label.keys())
+
+                elif key_status == 'released' and dragging == False:
+                    key_status = ''
+
+                plt.draw()
+
+            def when_leaving_axes(event):
+                # Delete the dinamic line line when the mouse leaves the axes
+                for line in ax.lines:
+                    if line.get_color() == 'g':
+                        line.remove()
+                plt.draw()
+
+            # Connect the click and move events to the on_click and on_move functions
+            fig.canvas.mpl_connect('button_press_event', on_click)
+            fig.canvas.mpl_connect('motion_notify_event', on_move)
+            fig.canvas.mpl_connect('axes_leave_event', when_leaving_axes)
+
 
 plt.show()
 
-print(points)
+print(new_points)
